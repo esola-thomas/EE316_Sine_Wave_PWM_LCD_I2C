355f0acce111b4df33433d0e09ea011ead3a98ad
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-02-06 01:16:44 -0500

    Basic counter source files

---
+++ src/Counter/counter/counter.vhd
@@ -0,0 +1,68 @@
+library IEEE;
+USE ieee.std_logic_1164.all;
+use IEEE.NUMERIC_STD.ALL;
+
+entity counter is
+	generic(
+		count_size 			: integer := 8; -- Output bus size
+		max_count			: integer := 15;
+		count_step_size 	: integer := 1;
+		count_delay			: integer := 49999999 -- Clk cycles delay until next count
+	);
+	port(
+		clk 		: in std_logic; 
+		ireset		: in std_logic;
+		direction 	: in std_logic; -- 1 Counts up, 0 counts down
+		carry_out	: out std_logic := '0'; -- Works for both counting up and conting down
+		count_out 	: out std_logic_vector (count_size-1 downto 0) := (others => '0')
+	);
+end counter;
+
+architecture arch of counter is
+
+	signal en : std_logic := '0';
+	signal clk_en_count : integer := 0;
+	signal count_int_reg: integer := 0;
+
+begin
+
+	clk_enabler : process (clk) begin
+		if (rising_edge(clk)) then
+			if (clk_en_count = count_delay) then
+				en <= '1';
+				clk_en_count <= 0;
+			else 
+				en <= '0';
+				clk_en_count <= clk_en_count + 1;
+			end if;
+		end if;
+	end process clk_enabler;
+	
+	main_count : process (clk, en) begin
+		if (ireset = '1') then -- Possible implementation, add if statement to reset to max or min value depending on the direction signal
+			count_int_reg <= 0;
+			carry_out <= '0';
+		elsif (rising_edge(clk) and en = '1') then
+			if (direction = '1') then -- Count up
+				if (count_int_reg + count_step_size > max_count) then
+					count_int_reg <= count_int_reg + count_step_size - max_count-1;
+					carry_out <= '1';
+				elsif (count_int_reg + count_step_size <= max_count) then
+					count_int_reg <= count_int_reg + count_step_size;
+					carry_out <= '0';
+				end if;	
+
+			elsif (direction = '0') then -- Count down
+				if (count_int_reg - count_step_size < 0) then 
+					count_int_reg <= count_int_reg - count_step_size + max_count+1;
+					carry_out <= '1';
+				elsif (count_int_reg - count_step_size >= 0) then
+					count_int_reg <= count_int_reg - count_step_size;
+					carry_out <= '0';
+				end if;
+			end if;
+		end if;
+	end process main_count;
+
+	count_out <= std_logic_vector(to_unsigned(count_int_reg, count_out'length));
+end arch;
---
+++ src/Counter/counter/counter_tb.vhd
@@ -0,0 +1,104 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity counter_tb is 
+end counter_tb;
+
+architecture arch_tb of counter_tb is
+
+constant count_out_size : integer := 8;
+component counter
+    generic(
+        count_size 			: integer := 8; -- Output bus size
+        max_count			: integer := 15;
+        count_step_size 	: integer := 1;
+        count_delay			: integer := 49999999 -- Clk cycles delay until next count
+    );
+    port(
+        clk 		: in std_logic; 
+        ireset		: in std_logic;
+        direction 	: in std_logic; -- 1 Counts up, 0 counts down
+        carry_out	: out std_logic := '0'; -- Works for both counting up and conting down
+        count_out 	: out std_logic_vector (count_size-1 downto 0) := (others => '0')
+    );
+end component;
+
+    signal clk, ireset, carry_out   : std_logic := '0';
+    signal direction                : std_logic := '1';
+    signal count_out                : std_logic_vector (count_out_size-1 downto 0);
+begin
+
+    -- c : counter 
+    -- generic map (count_size => 4, max_count => 5, count_step_size => 1 count_delay => 5); 
+    -- port map (clk => clk, ireset => ireset, direction => direction, count_out => count_out, carry_out => carry_out);
+
+    c : counter generic map (count_size => count_out_size, max_count => 10, count_step_size => 1, count_delay => 2)
+        port map (clk => clk, ireset => ireset, direction => direction, count_out => count_out, carry_out => carry_out);
+
+    clk <= not clk after 10 ns;
+
+    testbench : process begin 
+        -- 0
+        ireset      <= '0';
+        direction   <= '1';
+        wait for 100 ns;
+
+        -- 1
+        ireset      <= '0';
+        direction   <= '1';
+        wait for 100 ns;
+
+        -- 2
+        ireset      <= '0';
+        direction   <= '1';
+        wait for 100 ns;
+
+        -- 3
+        ireset      <= '0';
+        direction   <= '1';
+        wait for 100 ns;
+
+        -- 4
+        ireset      <= '0';
+        direction   <= '1';
+        wait for 100 ns;
+
+        -- 5
+        ireset      <= '0';
+        direction   <= '1';
+        wait for 100 ns;
+
+        -- 0
+        ireset      <= '0';
+        direction   <= '1';
+        wait for 100 ns;
+
+        -- 1
+        ireset      <= '0';
+        direction   <= '1';
+        wait for 100 ns;
+
+        -- 0
+        ireset      <= '0';
+        direction   <= '0';
+        wait for 100 ns;
+
+        -- 5
+        ireset      <= '0';
+        direction   <= '0';
+        wait for 100 ns;
+
+        -- 5
+        ireset      <= '0';
+        direction   <= '0';
+        wait for 100 ns;
+
+        -- 5
+        ireset      <= '0';
+        direction   <= '0';
+        wait for 100 ns;
+
+        wait;
+    end process testbench;
+end arch_tb;
